39e151604fb2bdc361b35864fac63f5d0175fd6a
Pugsworth <pworthwellington@gmail.com>
2020-09-29 15:27:23 -0600

    Bonemeal can now turn dirt into grass! BUG: particles don't appear for some reason

---
+++ src/main/java/net/pugsworth/manythings/mixin/BoneMealItemMixin.java
@@ -0,0 +1,44 @@
+package net.pugsworth.manythings.mixin;
+
+import javax.security.auth.callback.Callback;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+import me.sargunvohra.mcmods.autoconfig1u.shadowed.blue.endless.jankson.annotation.Nullable;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.Blocks;
+import net.minecraft.item.BoneMealItem;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Direction;
+import net.minecraft.world.World;
+import net.pugsworth.manythings.ManyThingsMod;
+
+@Mixin(BoneMealItem.class)
+public class BoneMealItemMixin {
+    
+    @Inject(method = "useOnGround", at = @At(value = "RETURN", ordinal = 1))
+    private static boolean useOnGround(ItemStack itemStack, World world, BlockPos blockPos, @Nullable Direction direction, CallbackInfoReturnable<Boolean> cir)
+    {
+        BlockPos affectedPos = blockPos.offset(direction.getOpposite());
+        BlockState blockState = world.getBlockState(affectedPos);
+        if (blockState.getBlock().equals(Blocks.DIRT))
+        {
+            if (!world.isClient && world.random.nextInt(4) == 0)
+            {
+                world.setBlockState(affectedPos, Blocks.GRASS_BLOCK.getDefaultState());
+                world.playLevelEvent(2005, blockPos, 0);
+            }
+
+            itemStack.decrement(1);
+            
+            
+            return true;
+        }
+
+        return false;
+    }
+}
